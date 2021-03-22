8877635858e8e12d3711b57fc0145a5a6ac9965b
mechaniccoder <yuseunghwan94@gmail.com>
2021-03-23 00:46:39 +0900

    Add route of adding form

--- routes/post.ts
+++ routes/post.ts
@@ -1,9 +1,28 @@
 import express, { Request, Response } from 'express';
+import { Post } from '../models/Post';
+import { User } from '../models/User';
 
 const router = express.Router();
 
-router.get('/', (req: Request, res: Response) => {
-  res.status(200).json({ id: 1, content: 'hi' });
+router.post('/', async (req: Request, res: Response) => {
+  try {
+    const { content, id } = req.body;
+
+    const post = await Post.create({
+      content: content,
+      UserId: id,
+    });
+    const user = await User.findOne({
+      where: {
+        id,
+      },
+    });
+
+    res.status(201).json({ ...post.get(), User: user });
+  } catch (err) {
+    console.log(err);
+    res.status(403).send('Fail to create Post');
+  }
 });
 
 router.get('/', (req: Request, res: Response) => {
